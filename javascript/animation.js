ddbcff3f8188507eb6ce7e889e9128329271990c
Hamaad Chughtai <Hamaad102@users.noreply.github.com>
2017-03-22 01:48:56 -0400

    Add files via upload

---
+++ javascript/animation.js
@@ -0,0 +1,29 @@
+$(window).scroll(function(){
+   
+    console.log($(this).scrollTop);
+    
+    if($(this).scrollTop()>=1100){
+        $('.one').addClass('animate');
+        $('.two').addClass('animate');
+        $('.three').addClass('animate');
+        $('.four').addClass('animate');
+        $('.five').addClass('animate');
+        $('.six').addClass('animate');
+    }
+    
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+}); 
